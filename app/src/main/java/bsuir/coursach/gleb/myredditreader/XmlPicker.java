564dc5d2b0dcfefa4a66dcd48fa1c9c19bc2c593
Gleb Kozinets <glebokator@gmail.com>
2018-12-24 12:37:02 +0300

    added htmltag extractor

--- app/src/main/java/bsuir/coursach/gleb/myredditreader/XmlPicker.java
+++ app/src/main/java/bsuir/coursach/gleb/myredditreader/XmlPicker.java
@@ -1,4 +1,25 @@
 package bsuir.coursach.gleb.myredditreader;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class XmlPicker {
+
+    private String TAG = "XML Extract";
+
+    private String htmlTag;
+    private String xmlLine;
+
+    public XmlPicker(String htmlTag, String xmlLine) {
+        this.htmlTag = htmlTag;
+        this.xmlLine = xmlLine;
+    }
+
+    public List<String> extract(){
+        List<String> result = new ArrayList<>();
+
+        String[] splitXML = xmlLine.split(htmlTag + "\"");
+
+        return result;
+    }
 }
